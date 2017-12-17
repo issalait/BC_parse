3621d574a2a2d31f5a2f278b18c65a469d02c792
issalait <issalait@gmail.com>
2017-12-18 00:48:18 +0200

    allure steps

--- src/test/java/appManager/BaseHelper.java
+++ src/test/java/appManager/BaseHelper.java
@@ -25,17 +25,17 @@ public class BaseHelper {
             return false;
         }
     }
-    @Step
+    @Step("clicks on el by css {cssSelector}")
     public void clickOnElByCss(String cssSelector) {
         wd.findElement(By.cssSelector(cssSelector)).click();
     }
 
-    @Step
+    @Step("get text by xpath {xpathExpression}")
     public String getTextByXpath(String xpathExpression) {
         return wd.findElement(By.xpath(xpathExpression)).getText();
     }
 
-    @Step
+    @Step("get text by id {elementID}")
     public String getTextByID(String elementID) {
         return wd.findElement(By.id(elementID)).getText();
     }
@@ -45,12 +45,12 @@ public class BaseHelper {
         return wd.findElement(By.id(elementName)).getText();
     }
 
-    @Step
+    @Step("clicks on el by Xpath {xpathExpression}")
     public void clickOnElByXpath(String xpathExpression) {
         wd.findElement(By.xpath(xpathExpression)).click();
     }
 
-    @Step
+    @Step("clicks on el by ID {elementID}")
     public void clickOnElByID(String elementID) {
         wd.findElement(By.id(elementID)).click();
     }
@@ -60,47 +60,47 @@ public class BaseHelper {
         wd.findElement(By.id(linkText)).click();
     }
 
-    @Step
+    @Step("type text by id {elementID}/{textToInput}")
     public void typeTextIntoElementByID(String elementID, String textToInput) {
         clickOnElByID(elementID);
         wd.findElement(By.id(elementID)).sendKeys(textToInput);
     }
 
-    @Step
+    @Step("clearAndTypeTextIntoElementByID text by id {elementID}/{textToInput}")
     public void clearAndTypeTextIntoElementByID(String elementID, String textToInput) {
         clickOnElByID(elementID);
         clearElementByID(elementID);
         wd.findElement(By.id(elementID)).sendKeys(textToInput);
     }
 
-    @Step
+    @Step("type text by Name {elementName}/{textToInput}")
     public void typeTextIntoElementByName(String elementName, String textToInput) {
         clickOnElementByName(elementName);
         wd.findElement(By.name(elementName)).sendKeys(textToInput);
     }
 
-    @Step
+    @Step("type text by Xpath {elementXpath}/{textToInput}")
     public void typeTextIntoElementByXpath(String elementXpath, String textToInput) {
         clickOnElementByXpath(elementXpath);
         wd.findElement(By.xpath(elementXpath)).sendKeys(textToInput);
     }
 
-    @Step
+    @Step("clear el by Name {elementName}")
     public void clearElementByName(String elementName) {
         wd.findElement(By.name(elementName)).clear();
     }
 
-    @Step
+    @Step("clear el by ID {elementID}")
     public void clearElementByID(String elementID) {
         wd.findElement(By.id(elementID)).clear();
     }
 
-    @Step
+    @Step("clicks on el by Name {elementName}")
     public void clickOnElementByName(String elementName) {
         wd.findElement(By.name(elementName)).click();
     }
 
-    @Step
+    @Step("clicks on el by Xpath {elementXpath}")
     public void clickOnElementByXpath(String elementXpath) {
         wd.findElement(By.xpath(elementXpath)).click();
     }
